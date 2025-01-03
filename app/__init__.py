5dc6551d99460ba7865d4e8755128ce335d355a3
Francisco Ribeiro <kikoribeiro95@gmail.com>
2025-01-03 16:53:55 +0000

    setup database initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from app.config import DevelopmentConfig
+from app.db.db import init_db
 from .apis.student import api as student
 from .apis.hello import api as hello
 from flask_restx import Api
@@ -9,6 +10,10 @@ from flask_restx import Api
 def create_app(config=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    # Initialize the database client: mongomock for unit tests, pymongo for development and production.
+    # That is, in-memory mock for unit tests, real MongoDB for integration tests, development, and production.
+    init_db(app.config["MONGO_URI"])
 
     api = Api(
         title="JobBoard",
--- app/db/db.py
+++ app/db/db.py
@@ -1,16 +1,32 @@
-from pymongo import MongoClient
+import pymongo
+import mongomock
 
-mongo_client = None
-demo_db = "DEMO_DB"
+# Note: see Flask's context management for an alternative implementation.
+# Did not use it here because I thought it could abstract too much away.
 
+class DatabaseClient:
+    def __init__(self, uri):
+        if uri.startswith("mongomock://"):
+            self.client = mongomock.MongoClient()
+        else:
+            self.client = pymongo.MongoClient(uri)
 
-def get_db_client():
-    global mongo_client
-    if not mongo_client:
-        mongo_client = MongoClient()  # defaults to localhost:27017
-    return mongo_client
+    def get_db(self, db_name):
+        return self.client[db_name]
 
+# Global variable to hold the database client instance
+db_client = None
+
+def init_db(uri):
+    global db_client
+    db_client = DatabaseClient(uri)
 
 def get_db():
-    client = get_db_client()
-    return client[demo_db]
+    if db_client is None:
+        raise Exception("Database client is not initialized. Call init_db(uri) first.")
+    db_name = "test_db" if db_client.client.address == ("localhost", 27017) else "prod_db"
+    return db_client.get_db(db_name)
+
+def get_collection(collection_name):
+    db = get_db()
+    return db[collection_name]
