15e58844f1a9473de09bbd2314de2d37c50923e8
Sarah Nadi <nadi@ualberta.ca>
2025-01-03 10:56:01 +0400

    Use pytest-mongodb

--- tests/test_student.py
+++ tests/test_student.py
@@ -5,3 +5,9 @@ def test_list_students(client):
     response = client.get("/students/")
     assert response.status_code == 200
     # assert response.json == {"message": "students!"}
+
+
+def test_students(mongodb):
+    assert "students" in mongodb.list_collection_names()
+    manuel = mongodb.students.find_one({"name": "Sarah"})
+    assert manuel["email"] == "sarah@nyu.edu"
