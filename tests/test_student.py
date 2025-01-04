5433a355299a0e137e2e85702b3580c1ad2d1df7
Francisco Ribeiro <kikoribeiro95@gmail.com>
2025-01-04 00:26:03 +0000

    - populate mock db with data in students.yaml; - adapt unit tests according to changes so far.

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,17 +1,31 @@
-import pytest
-import mongomock
-from bson.json_util import dumps
-
-
-def test_list_students(client, mongodb):
+# Does the following test make sense?
+# That is, it seems like something to make sure the test setup is working and not so much about testing the application.
+# Good for sanity check, I guess?
+def test_list_students(client):
+    """
+    Test that all seeded students are returned by the students/ endpoint.
+    """
+    # Call the list students API endpoint
     response = client.get("/students/")
+
     assert response.status_code == 200
-    assert response.json == dumps(mongodb.students.find())
+
+    data = response.json
+    assert isinstance(data, list)
+    assert len(data) == 2  # We seeded two students in students.yaml
+
+    # Validate content
+    expected_students = [
+        {"name": "Sarah", "email": "sarah@nyu.edu", "seniority": "senior"},
+        {"name": "Manuel", "email": "manuel@nyu.edu", "seniority": "sophomore"},
+    ]
+
+    for student in expected_students:
+        assert student in data
 
 
-@mongomock.patch(servers=(("server.example.com", 27017),))
-def test_get_student(client):
-    response = client.get("students/sarah%40nyu.edu")
+def test_get_existing_student(client):
+    response = client.get("students/sarah@nyu.edu")
     assert response.status_code == 200
     assert response.json == {
         "name": "Sarah",
@@ -20,7 +34,13 @@ def test_get_student(client):
     }
 
 
-def test_students(mongodb):
-    assert "students" in mongodb.list_collection_names()
-    manuel = mongodb.students.find_one({"name": "Sarah"})
+def test_get_non_existing_student(client):
+    response = client.get("students/non_existent@nyu.edu")
+    assert response.status_code == 404
+    assert response.json == "Student not found"
+
+
+def test_students(seeded_student_db):
+    assert "students" in seeded_student_db.list_collection_names()
+    manuel = seeded_student_db.students.find_one({"name": "Sarah"})
     assert manuel["email"] == "sarah@nyu.edu"
